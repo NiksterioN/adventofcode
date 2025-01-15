a1324e14e096a98f9f8ed2f77d3a9673a6f06aa1
NiksterioN <paulnikobautista@gmail.com>
2025-01-15 13:29:21 +0800

    Add AoC2015 Day3 Part2 Solution

--- 2015/day3/main.cpp
+++ 2015/day3/main.cpp
@@ -8,37 +8,37 @@
 using Point = std::pair<int, int>;
 
 // Moves the point north (^), south (v), east (>), or west (<)
-Point movePoint(const Point &point, const char &direction)
+Point movePoint(const Point& point, const char& direction)
 {
     Point currentPoint = point;
     switch (direction)
     {
-        case '^':
-            ++currentPoint.second;
-            break;
-        case 'v':
-            --currentPoint.second;
-            break;
-        case '<':
-            --currentPoint.first;
-            break;
-        case '>':
-            ++currentPoint.first;
-            break;
-        default:
-            break;
+    case '^':
+        ++currentPoint.second;
+        break;
+    case 'v':
+        --currentPoint.second;
+        break;
+    case '<':
+        --currentPoint.first;
+        break;
+    case '>':
+        ++currentPoint.first;
+        break;
+    default:
+        break;
     }
     return currentPoint;
 }
 
 // Returns a vector containing the visited points in std::pair<x, y>
-std::vector<Point> createVisitedPoints(const std::string &listOfDirection)
+std::vector<Point> createVisitedPoints(const std::string& listOfDirection)
 {
     Point currentPoint(0, 0);
-    std::vector<Point> listOfPoints({currentPoint});
+    std::vector<Point> listOfPoints({ currentPoint });
     listOfPoints.reserve(listOfDirection.size());
 
-    for (const char &direction : listOfDirection)
+    for (const char& direction : listOfDirection)
     {
         Point newPoint = movePoint(currentPoint, direction);
         listOfPoints.push_back(newPoint);
@@ -50,32 +50,55 @@ std::vector<Point> createVisitedPoints(const std::string &listOfDirection)
 
 bool testFunctions()
 {
-    Point a(0, 0);
     const std::string testDirectionA = "^";
     const std::string testDirectionB = "^v";
     const std::string testDirectionC = "";
 
-    assert(movePoint(Point(0, 0), '^') == Point(0,  1));
+    assert(movePoint(Point(0, 0), '^') == Point(0, 1));
     assert(movePoint(Point(0, 0), 'v') == Point(0, -1));
     assert(movePoint(Point(0, 0), '<') == Point(-1, 0));
-    assert(movePoint(Point(0, 0), '>') == Point(1,  0));
+    assert(movePoint(Point(0, 0), '>') == Point(1, 0));
 
-    assert(createVisitedPoints(testDirectionA) == std::vector<Point>({Point(0, 0), Point(0, 1)}));
-    assert(createVisitedPoints(testDirectionB) == std::vector<Point>({Point(0, 0), Point(0, 1), Point(0, 0)}));
-    assert(createVisitedPoints(testDirectionC) == std::vector<Point>({Point(0, 0)}));
+    assert(createVisitedPoints(testDirectionA) == std::vector<Point>({ Point(0, 0), Point(0, 1) }));
+    assert(createVisitedPoints(testDirectionB) == std::vector<Point>({ Point(0, 0), Point(0, 1), Point(0, 0) }));
+    assert(createVisitedPoints(testDirectionC) == std::vector<Point>({ Point(0, 0) }));
     return true;
+}
+
+// Returns TRUE if the number is even, otherwise returns FALSE.
+inline bool isEven(const int &n)
+{
+    return n % 2;
 }
 
 int main(void)
 {
-//    testFunctions();
+    //    testFunctions();
     std::string listOfDirection;
     std::cin >> listOfDirection;
 
     std::vector<Point> listOfVisitedPoints = createVisitedPoints(listOfDirection);
 
     std::set<Point> listOfUniqueVisitedPoints(listOfVisitedPoints.begin(), listOfVisitedPoints.end());
-    std::cout << "Delivered atleast "<< listOfUniqueVisitedPoints.size() << std::endl;
+    std::cout << "Santa delivered at least " << listOfUniqueVisitedPoints.size() << " presents." << std::endl;
+
+    //Santa and Robo-Santa start at the same location (delivering two presents to the same starting house), then take turns moving based on instructions
+
+    std::string listOfSantaDirection;
+    std::string listofRoboSantaDirection;
+    for (int i = 0; i < listOfDirection.size(); ++i)
+    {
+        if (isEven(i)) listofRoboSantaDirection += listOfDirection[i];
+        else listOfSantaDirection += listOfDirection[i];
+    }
+
+    std::vector<Point> listOfSantaVisitedPoints = createVisitedPoints(listOfSantaDirection);
+    std::vector<Point> listOfRoboSantaVisitedPoints = createVisitedPoints(listofRoboSantaDirection);
+
+    std::set<Point> listOfBothUniqueVisitedPoints(listOfSantaVisitedPoints.begin(), listOfSantaVisitedPoints.end());
+    listOfBothUniqueVisitedPoints.insert(listOfRoboSantaVisitedPoints.begin(), listOfRoboSantaVisitedPoints.end());
+
+    std::cout << "Santa and Robo-Santa delivered at least " << listOfBothUniqueVisitedPoints.size() << " presents." << std::endl;
 
     return 0;
 }
