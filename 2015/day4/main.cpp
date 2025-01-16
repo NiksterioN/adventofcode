0fbc92dbc54f19841cc2ff79f577f6027572f9ff
NiksterioN <paulnikobautista@gmail.com>
2025-01-16 08:45:57 +0800

    AoC Day4 - Add Solution to Part2 and fix off-by-one error

--- 2015/day4/main.cpp
+++ 2015/day4/main.cpp
@@ -3,6 +3,7 @@
 #include <cassert>
 #include "src/hashlibpp.h"
 
+// Returns the MD5 hash of a given string
 static std::string generateMD5Hash(const std::string &key)
 {
 	hashwrapper* h = new md5wrapper();
@@ -12,17 +13,18 @@ static std::string generateMD5Hash(const std::string &key)
 	return md5;
 }
 
-static long findLowestPositiveHashNumber(std::string key)
+// Returns the lowest positive number with an MD5 Hash starting with "matchString"
+static long findLowestPositiveHashNumber(const std::string &key, const std::string &matchString)
 {
 	long startNum = -1;
 	std::string newMD5Hash, combinationKey;
 	
 	do
 	{
+		++startNum;
 		combinationKey = key + std::to_string(startNum);
 		newMD5Hash = generateMD5Hash(combinationKey);
-		++startNum;
-	} while (newMD5Hash.substr(0, 5) != "00000");
+	} while (newMD5Hash.substr(0, matchString.length()) != matchString);
 
 	return startNum;
 
@@ -33,9 +35,8 @@ static bool testFunctions()
 	std::string keyA = "abcdef";
 	std::string keyB = "pqrstuv";
 
-
-	assert(findLowestPositiveHashNumber(keyA) == 609043);
-	assert(findLowestPositiveHashNumber(keyB) == 1048970);
+	assert(findLowestPositiveHashNumber(keyA, std::string("00000")) == 609043);
+	assert(findLowestPositiveHashNumber(keyB, std::string("00000")) == 1048970);
 
 	return true;
 }
@@ -47,11 +48,8 @@ int main(void)
 	std::cout << "Input Key: " << std::endl;
 	std::cin >> secretKey;
 
-	//std::cout << generateMD5Hash(secretKey) << std::endl;
-	std::cout << "Lowest Positive Hash Number with MD5 starting at 00000: " << findLowestPositiveHashNumber(secretKey) << std::endl;
-
+	std::cout << "Lowest Positive Hash Number with MD5 starting at 00000: " << findLowestPositiveHashNumber(secretKey, std::string("00000")) << std::endl;
+	std::cout << "Lowest Positive Hash Number with MD5 starting at 000000: " << findLowestPositiveHashNumber(secretKey, std::string("000000")) << std::endl;
 	
-
-
 	return 0;
 }
