26ed291b8320b568cd2db203bfb9ac9f908d1009
NiksterioN <paulnikobautista@gmail.com>
2025-01-17 09:45:50 +0800

    AoC 2015 Day 5 - Add Solution to Part 1

---
+++ 2015/day5/main.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <cctype>
+#include <cassert>
+#include <string>
+#include <vector>
+#include <set>
+
+// Returns TRUE if a character is a vowel, otherwise returns FALSE
+static bool isVowel(const char &c)
+{
+    const char tempChar = tolower(c);
+    return (tempChar == 'a' || tempChar == 'e' || tempChar == 'i' || tempChar == 'o' || tempChar == 'u');
+}
+
+// Returns TRUE if a string contains at least N vowels, otherwise returns FALSE
+static bool containsNVowels(const std::string &word, const int n)
+{
+    int vowelCount = 0;
+    for (const char& c : word)
+    {
+        if (isVowel(c))
+            ++vowelCount;
+        if (vowelCount == n)
+            return true;
+    }
+    return false;
+}
+
+// Returns TRUE if a string contains N letters in a row, otherwise returns FALSE
+static bool containsNLettersInARow(const std::string &word, const int &n)
+{
+    const int indexDifference = n - 1;
+    for (std::size_t currentIndex = 0; currentIndex < word.length() - 1; ++currentIndex)
+    {
+        char currentChar = word[currentIndex];
+        std::size_t secondIndex = word.find(currentChar, currentIndex + 1);
+
+        if ((secondIndex - currentIndex) == indexDifference)
+            return true;
+    }
+   
+    return false;
+}
+
+// Returns TRUE if a string contains XX Letters, otherwise returns FALSE
+static bool containsXXLetters(const std::string& word)
+{
+    static std::vector<std::string> listOfExcludedWords{ {"ab"}, {"cd"}, {"pq"}, {"xy"} };
+    bool wordFlag = false;
+    for (const auto &excludedWords : listOfExcludedWords)
+    {
+        if (word.find(excludedWords) != std::string::npos)
+            wordFlag = true;
+    }
+
+    return wordFlag;
+}
+
+// Returns TRUE if a string is nice, otherwise FALSE.
+/*
+A "nice" string is one with all of the following properties:
+
+    It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
+    It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+    It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements
+
+Otherwise, the string is as a "naughty" string
+*/
+static bool isNiceString(const std::string &word)
+{   
+    return containsNVowels(word, 3) && containsNLettersInARow(word, 2) && !containsXXLetters(word);
+}
+
+// Returns the number of nice strings in a listOfString
+static int countNiceString(const std::vector<std::string> listOfString)
+{
+    int niceStringCounter = 0;
+    
+    for (const auto& word : listOfString)
+    {
+        if (isNiceString(word))
+            ++niceStringCounter;
+    }
+
+    return niceStringCounter;
+}
+
+static bool testFunctions()
+{
+    std::string wordA("ugknbfddgicrmopn");  // nice
+    std::string wordB("aaa");               // nice    
+    std::string wordC("jchzalrnumimnmhp");  // naughty
+    std::string wordD("haegwjzuvuyypxyu");  // naughty
+    std::string wordE("dvszwmarrgswjxmb");  // naughty
+
+    std::vector<std::string> lowA{wordA, wordB, wordC, wordD, wordE};
+
+    assert(containsNVowels(wordA, 2) == true);
+    assert(containsNVowels(wordE, 2) == false);
+
+    assert(containsNLettersInARow(wordA, 2) == true);
+    assert(containsNLettersInARow(wordC, 2) == false);
+
+    assert(containsXXLetters(wordA) == false);
+    assert(containsXXLetters(wordD) == true);
+
+
+    assert(isNiceString(wordA) == true);
+    assert(isNiceString(wordB) == true);
+    assert(isNiceString(wordC) == false); // no double letter
+    assert(isNiceString(wordD) == false); // contains string xy
+    assert(isNiceString(wordE) == false); // contains only one vowel
+
+    assert(countNiceString(lowA) == 2);
+
+    std::cout << "Test Pass" << std::endl;
+    return true;
+}
+
+int main()
+{
+    //testFunctions();
+    
+    std::vector<std::string> listOfString;
+    listOfString.reserve(1000);
+    std::string direction;
+    while (std::cin >> direction)
+    {
+        listOfString.push_back(direction);
+    }
+    
+    std::cout << "Nice Strings:  " << countNiceString(listOfString) << std::endl;
+
+    return 0;
+}
