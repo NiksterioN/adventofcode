202d4efa48695e71a7f835eaab10902abf0172ee
NiksterioN <paulnikobautista@gmail.com>
2025-01-17 14:33:39 +0800

    AoC 2015 Day 5 - Initial Code for Part 2

--- 2015/day5/main.cpp
+++ 2015/day5/main.cpp
@@ -26,10 +26,10 @@ static bool containsNVowels(const std::string &word, const int n)
     return false;
 }
 
-// Returns TRUE if a string contains N letters in a row, otherwise returns FALSE
-static bool containsNLettersInARow(const std::string &word, const int &n)
+// Returns TRUE if a string contains a pair of any two same letters with N letters in between
+static bool containsNLettersBetweenXLetters(const std::string &word, const int &n)
 {
-    const int indexDifference = n - 1;
+    const int indexDifference = n + 1;
     for (std::size_t currentIndex = 0; currentIndex < word.length() - 1; ++currentIndex)
     {
         char currentChar = word[currentIndex];
@@ -68,7 +68,7 @@ Otherwise, the string is as a "naughty" string
 */
 static bool isNiceString(const std::string &word)
 {   
-    return containsNVowels(word, 3) && containsNLettersInARow(word, 2) && !containsXXLetters(word);
+    return containsNVowels(word, 3) && containsNLettersBetweenXLetters(word, 0) && !containsXXLetters(word);
 }
 
 // Returns the number of nice strings in a listOfString
@@ -85,7 +85,7 @@ static int countNiceString(const std::vector<std::string> listOfString)
     return niceStringCounter;
 }
 
-static bool testFunctions()
+static bool testFunctionPartA()
 {
     std::string wordA("ugknbfddgicrmopn");  // nice
     std::string wordB("aaa");               // nice    
@@ -98,8 +98,8 @@ static bool testFunctions()
     assert(containsNVowels(wordA, 2) == true);
     assert(containsNVowels(wordE, 2) == false);
 
-    assert(containsNLettersInARow(wordA, 2) == true);
-    assert(containsNLettersInARow(wordC, 2) == false);
+    assert(containsNLettersBetweenXLetters(wordA, 0) == true);
+    assert(containsNLettersBetweenXLetters(wordC, 0) == false);
 
     assert(containsXXLetters(wordA) == false);
     assert(containsXXLetters(wordD) == true);
@@ -113,14 +113,39 @@ static bool testFunctions()
 
     assert(countNiceString(lowA) == 2);
 
-    std::cout << "Test Pass" << std::endl;
+    std::cout << __func__ << ": Test Pass" << std::endl;
     return true;
 }
 
+// Returns TRUE if a string contains a pair of any two letters
+// that appears at least twice in the string without overlapping
+// Otherwise, returns FALSE
+static bool containsTwoXXLetters(const std::string &word)
+{
+    return true;
+}
+
+static bool testFunctionPartB()
+{
+    std::string wordA("qjhvhtzxzqqjkmpb");  // nice
+    std::string wordB("xxyxx");             // nice
+    std::string wordC("uurcxstgmygtbstg");  // naughty
+    std::string wordD("ieodomkazucvgmuy");  // naughty
+
+    assert(containsNLettersBetweenXLetters("xyxy", 1) == true);
+    assert(containsNLettersBetweenXLetters("aaa", 1) == false);
+
+    std::cout << __func__ << ": Test Pass" << std::endl;
+
+    return true;
+}
+
+
+
 int main()
 {
-    //testFunctions();
-    
+    //testFunctionPartA();
+    testFunctionPartB();
     std::vector<std::string> listOfString;
     listOfString.reserve(1000);
     std::string direction;
