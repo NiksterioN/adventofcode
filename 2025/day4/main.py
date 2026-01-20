e9c2a611aeba6a3251511b651239fafd24c69325
NiksterioN <paulnikobautista@gmail.com>
2026-01-20 15:58:54 +0800

    AoC 2025 - Day 3 - Refactor In-progress

--- 2025/day4/main.py
+++ 2025/day4/main.py
@@ -2,95 +2,104 @@ from collections import deque
 import time
 
 
-def traverse_matrix(matrix):
-    """
-    Iterates over the matrix to solve the number of accessible rolls.
-    A roll is accessible if there are fewer than four (4) paper rolls in the eight adjacent positions.
-    :param matrix: The matrix or diagram.
-    For example in the matrix below, '@' denotes a paper roll and '.' denotes an empty space.
-    ['..@@.@',
-     '@@@.@.',
-     '@@@@@.']
+class Matrix:
+    def __init__(self, matrix):
+        self.original_matrix = matrix.copy()
+        self.current_matrix = matrix.copy()
+        self.next_matrix = []
+        self.number_of_row = len(self.current_matrix)
+        self.number_of_column = len(self.current_matrix[0])
 
-    :return: (The matrix with paper rolls removed, The number of accessible paper rolls)
-    """
+    def reset(self):
+        self.current_matrix = self.original_matrix.copy()
 
-    number_of_row = len(matrix)
-    number_of_column = len(matrix[0])
-    total_accessible_rolls = 0
-    new_matrix = []
+    def print(self):
+        print(self.current_matrix)
 
-    for r in range(number_of_row):
-        new_column = []
-        for c in range(number_of_column):
-            if matrix[r][c] == "@" and is_accessible_by_forklift(matrix, (r, c)):
-                total_accessible_rolls = total_accessible_rolls + 1
-                new_column.append(".")
-            else:
-                new_column.append(matrix[r][c])
-        new_matrix.append(new_column)
-    return (new_matrix, total_accessible_rolls)
+    @property
+    def traverse_matrix(self):
+        """
+        Iterates over the matrix to solve the number of accessible rolls.
+        A roll is accessible if there are fewer than four (4) paper rolls in the eight adjacent positions.
 
+        For example in the matrix below, '@' denotes a paper roll and '.' denotes an empty space.
+        ['..@@.@',
+         '@@@.@.',
+         '@@@@@.']
 
-def traverse_matrix_recursively(matrix):
-    """
-    Recursively iterates to solve the number of accessible rolls within the matrix (with each accessible paper rolls removed per call),
-    until there is no more accessible rolls.
+        :return: The number of accessible paper rolls
+        """
+        total_accessible_rolls = 0
 
-    :param matrix: The matrix or diagram.
-    :return: The total number of rolls that can be removed
-    """
-    total_number_of_rolls = 0
-    new_matrix, current_number_of_rolls = traverse_matrix(matrix)
-    total_number_of_rolls = current_number_of_rolls
-    while current_number_of_rolls > 0:
-        new_matrix, current_number_of_rolls = traverse_matrix(new_matrix)
-        total_number_of_rolls = total_number_of_rolls + current_number_of_rolls
+        for r in range(self.number_of_row):
+            new_row = []
+            for c in range(self.number_of_column):
+                if self.current_matrix[r][c] == "@" and self.is_accessible_by_forklift((r, c)):
+                    total_accessible_rolls = total_accessible_rolls + 1
+                    new_row.append(".")
+                else:
+                    new_row.append(self.current_matrix[r][c])
 
-    return total_number_of_rolls
+            self.next_matrix.append(["".join(new_row)])
 
+        self.current_matrix = self.next_matrix.copy()
+        self.next_matrix = []
+        return total_accessible_rolls
 
-def is_accessible_by_forklift(matrix, coordinate):
-    """
-    Checks if there are fewer than four (4) paper rolls in the eight adjacent positions.
-    :param matrix: The matrix or diagram
-    :param coordinate: (r, c) : The position to check in the matrix
+    @property
+    def traverse_matrix_recursively(self):
+        """
+        Recursively iterates to solve the number of accessible rolls within the matrix (with each accessible paper rolls removed per call),
+        until there is no more accessible rolls to remove.
 
-    :return:    True if the there are fewer than four (4) paper rolls in the eight adjacent positions.
-                Otherwise, returns False
-    """
-    if get_adjacent_sum(matrix, coordinate) < 4:
-        return True
-    else:
-        return False
+        :return: The total number of rolls that can be removed
+        """
+        total_number_of_rolls = 0
+        current_number_of_rolls = self.traverse_matrix
+        total_number_of_rolls = current_number_of_rolls
+        while current_number_of_rolls > 0:
+            current_number_of_rolls = self.traverse_matrix
+            total_number_of_rolls = total_number_of_rolls + current_number_of_rolls
 
+        return total_number_of_rolls
 
-def get_adjacent_sum(matrix, coordinates):
-    """
-    Get the number of paper rolls in the eight adjacent positions given in the coordinates (excluding the coordinate itself)
-    For example in the given below 
-    ['..@',
-     '@@@',
-     '@@@']
-    :param matrix: The matrix or diagram
-    :param coordinates: (r, c) : The position to check in the matrix
-    :return: The number of paper rolls in the eight adjacent positions given in the coordinates (excluding the coordinate itself)
-    """
-    number_of_adjacent_rolls = 0
-    number_of_row = len(matrix)
-    number_of_column = len(matrix[0])
-    for r in range(coordinates[0] - 1, coordinates[0] + 2):
-        for c in range(coordinates[1] - 1, coordinates[1] + 2):
-            # Boundary conditions
-            if r >= number_of_row or r < 0:
-                continue
-            if c >= number_of_column or c < 0:
-                continue
-            if (r, c) == coordinates:
-                continue
-            if matrix[r][c] == "@":
-                number_of_adjacent_rolls = number_of_adjacent_rolls + 1
-    return number_of_adjacent_rolls
+    def is_accessible_by_forklift(self, coordinate):
+        """
+        Checks if there are fewer than four (4) paper rolls in the eight adjacent positions.
+        :param matrix: The matrix or diagram
+        :param coordinate: (r, c) : The position to check in the matrix
+
+        :return:    True if the there are fewer than four (4) paper rolls in the eight adjacent positions.
+                    Otherwise, returns False
+        """
+        if self.get_adjacent_sum(coordinate) < 4:
+            return True
+        else:
+            return False
+
+    def get_adjacent_sum(self, coordinates):
+        """
+        Get the number of paper rolls in the eight adjacent positions given in the coordinates (excluding the coordinate itself)
+        For example in the given below
+        ['..@',
+         '@@@',
+         '@@@']
+        :param coordinates: (r, c) : The position to check in the matrix
+        :return: The number of paper rolls in the eight adjacent positions given in the coordinates (excluding the coordinate itself)
+        """
+        number_of_adjacent_rolls = 0
+        for r in range(coordinates[0] - 1, coordinates[0] + 2):
+            for c in range(coordinates[1] - 1, coordinates[1] + 2):
+                # Boundary conditions
+                if r >= self.number_of_row or r < 0:
+                    continue
+                if c >= self.number_of_column or c < 0:
+                    continue
+                if (r, c) == coordinates:
+                    continue
+                if self.current_matrix[r][c] == "@":
+                    number_of_adjacent_rolls = number_of_adjacent_rolls + 1
+        return number_of_adjacent_rolls
 
 
 def test_function():
@@ -105,13 +114,18 @@ def test_function():
               .@@@@@@@@.\n
               @.@.@@@.@."""
 
-    grid = grid.split();
-    assert (get_adjacent_sum(grid, (0, 0)) == 2)
-    assert (get_adjacent_sum(grid, (1, 1)) == 6)
-    assert (get_adjacent_sum(grid, (len(grid) - 1, len(grid[0]) - 1)) == 2)
+    grid = grid.split()
+    test_matrix = Matrix(grid)
 
-    assert (traverse_matrix(grid)[1] == 13)
-    assert (traverse_matrix_recursively(grid) == 43)
+    assert (test_matrix.get_adjacent_sum((0, 0)) == 2)
+    assert (test_matrix.get_adjacent_sum((1, 1)) == 6)
+    assert (test_matrix.get_adjacent_sum((len(grid) - 1, len(grid[0]) - 1)) == 2)
+
+    assert (test_matrix.traverse_matrix == 13)
+    test_matrix.reset()
+    assert (test_matrix.traverse_matrix_recursively == 43)
+
+    print("test done")
     return True
 
 
@@ -121,10 +135,10 @@ def main():
     filename = "in.txt"
     with open(filename, "r") as file:
         matrix = file.read().split()
-
+        puzzle_matrix = Matrix(matrix)
         start = time.time()
-        print(traverse_matrix(matrix)[1])
-        print(traverse_matrix_recursively(matrix))
+        print(puzzle_matrix.traverse_matrix)
+        print(puzzle_matrix.traverse_matrix_recursively)
         end = time.time()
 
         print(end - start)
